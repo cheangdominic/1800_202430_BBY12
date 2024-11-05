8f5c23917698841670f281be67feb45ad95a5676
Dominic <dcheang@my.bcit.ca>
2024-11-04 21:18:19 -0800

    Added placeholder HTML files for about and FAQ, functionality for the buttons, fixed auto logout for index.html

--- scripts/script.js
+++ scripts/script.js
@@ -15,7 +15,6 @@ function logout() {
     firebase.auth().signOut().then(() => {
         // Sign-out successful.
         console.log("logging out user");
-        redirectToPage('index.html');
       }).catch((error) => {
         // An error happened.
       });
