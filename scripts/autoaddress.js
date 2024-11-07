f86b6df07e5e55cf8b9fc238330a963cb6158751
Dominic <dcheang@my.bcit.ca>
2024-11-07 11:17:38 -0800

    Narrowed search types in park address

--- scripts/autoaddress.js
+++ scripts/autoaddress.js
@@ -1,19 +1,14 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiZGNoZWFuZyIsImEiOiJjbTM3aXVka3YwZ2lpMmlwd2VndTN0NWw4In0.UNRVJNRE_fuqrK5LtRYHKg';
+
 const geocoder = new MapboxGeocoder({
     accessToken: mapboxgl.accessToken,
-    types: 'country,region,place,postcode,locality,neighborhood'
+    type: 'poi,place'
 });
 
 geocoder.addTo('#geocoder');
 
-let selectedAddress = "";
-
-geocoder.on('result', (event) => {
-    selectedAddress = event.result.place_name;
-    console.log(`Selected Address: ${selectedAddress}`);
-});
+const results = document.getElementById('result');
 
 geocoder.on('clear', () => {
-    selectedAddress = "";
     results.innerText = '';
 });
