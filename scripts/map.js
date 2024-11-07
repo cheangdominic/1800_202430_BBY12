7e544f85b18d319b5c16ec631b7fbbb09b65d9e2
Dominic <dcheang@my.bcit.ca>
2024-11-07 10:57:30 -0800

    Accepted coordinates in geocoder for map.html and marker placement

--- scripts/map.js
+++ scripts/map.js
@@ -4,7 +4,7 @@ const map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v12',
     center: [-24, 42],
-    zoom: 1
+    zoom: 1.75
 });
 
 const geolocate = new mapboxgl.GeolocateControl({
@@ -17,8 +17,86 @@ const geolocate = new mapboxgl.GeolocateControl({
 map.addControl(geolocate, 'bottom-right');
 
 
-const geocoder = new MapboxGeocoder({
-    accessToken: mapboxgl.accessToken,
-    mapboxgl: mapboxgl
-});
-map.addControl(geocoder, 'top-left');
+const coordinatesGeocoder = function (query) {
+    const matches = query.match(
+        /^[ ]*(?:Lat: )?(-?\d+\.?\d*)[, ]+(?:Lng: )?(-?\d+\.?\d*)[ ]*$/i
+    );
+    if (!matches) {
+        return null;
+    }
+
+    function coordinateFeature(lng, lat) {
+        return {
+            center: [lng, lat],
+            geometry: {
+                type: 'Point',
+                coordinates: [lng, lat]
+            },
+            place_name: 'Lat: ' + lat + ' Lng: ' + lng,
+            place_type: ['coordinate'],
+            properties: {},
+            type: 'Feature'
+        };
+    }
+
+    const coord1 = Number(matches[1]);
+    const coord2 = Number(matches[2]);
+    const geocodes = [];
+
+    if (coord1 < -90 || coord1 > 90) {
+        geocodes.push(coordinateFeature(coord1, coord2));
+    }
+
+    if (coord2 < -90 || coord2 > 90) {
+        geocodes.push(coordinateFeature(coord2, coord1));
+    }
+
+    if (geocodes.length === 0) {
+        geocodes.push(coordinateFeature(coord1, coord2));
+        geocodes.push(coordinateFeature(coord2, coord1));
+    }
+
+    return geocodes;
+};
+
+map.addControl(
+    new MapboxGeocoder({
+        accessToken: mapboxgl.accessToken,
+        localGeocoder: coordinatesGeocoder,
+        zoom: 14,
+        placeholder: 'Search',
+        mapboxgl: mapboxgl,
+        reverseGeocode: true,
+    }),
+);
+
+let currentMarker;
+
+currentMarker = new mapboxgl.Marker({
+    color: "FFFFFF",
+    draggable: true
+})
+
+map.on('click', (event) => {
+    const coordinates = event.lngLat;
+    
+    if(currentMarker) {
+        currentMarker.remove();
+    }
+
+    currentMarker = new mapboxgl.Marker()
+      .setLngLat(coordinates)
+      .addTo(map);
+      map.flyTo({
+        center: coordinates,
+        zoom: 14,
+        speed: 0.8,
+        easing: function (t) { return t; }
+      });
+
+      const geocoderInput = document.querySelector('.mapboxgl-ctrl-geocoder input');
+      if (geocoderInput) {
+          // Set the new value as a Lat, Lng format
+          geocoderInput.value = `Lat: ${coordinates.lat.toFixed(2)}, Lng: ${coordinates.lng.toFixed(2)}`;
+      }
+  });
