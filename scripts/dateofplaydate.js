44af553684fd68844f5547f6b996feaccb93f772
Dominic <dcheang@my.bcit.ca>
2024-11-12 00:18:53 -0800

    Made it so seperate words are capitalized in post names and made it so posts get removed from the database when over the date

--- scripts/dateofplaydate.js
+++ scripts/dateofplaydate.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     const year = now.getFullYear();
     const month = String(now.getMonth() + 1).padStart(2, '0');
-    const day = String(now.getDate() + 1).padStart(2, '0');
+    const day = String(now.getDate()).padStart(2, '0');
     const hours = String(now.getHours()).padStart(2, '0');
     const minutes = String(now.getMinutes()).padStart(2, '0');
     const minDateTime = `${year}-${month}-${day}T${hours}:${minutes}`;
