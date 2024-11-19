00df60808623a965305592ba2d566f00aed8b23c
trung058 <huutrung0508@gmail.com>
2024-11-19 14:33:49 -0800

    Added basic css for friends page

---
+++ scripts/friends.js
@@ -0,0 +1,13 @@
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCesnliQh3zmwEc782jk71-bCdPWVS8h2s",
+    authDomain: "updog-d7aa0.firebaseapp.com",
+    projectId: "updog-d7aa0",
+    storageBucket: "updog-d7aa0.firebasestorage.app",
+    messagingSenderId: "509822779265",
+    appId: "1:509822779265:web:171948f292612764ad3280"
+  };
+
